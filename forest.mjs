ad4f23bd3c79484c86345b65ae32c66909137e5d
menfin <menfin.swgoh@gmail.com>
2021-09-24 16:32:47 +0200

    forest: send all summoners level >= 2 + send to both forest contracts

--- forest.mjs
+++ forest.mjs
@@ -1,23 +1,23 @@
 // A tool to explore caves, adventure, and find riches.
 // https://github.com/dmptrluke/ramen
 
-import FOREST_ABI from "./forestabi.json";
+import FOREST_ABI_V2 from "./forestabi_v2.json";
+import FOREST_ABI_V1 from "./forestabi_v1.json";
+import RARITY_ABI from "./rarityabi.json";
 import {randomBytes} from "crypto";
 import Web3 from 'web3';
 
 import config from './config.json'
+import fetch from "node-fetch";
 
-const forest_address = "0xb37d3d79ea86B0334d9322c695339D577A3D57be";
-
-//const summoners = [774976, 775995];
-const summoners = [629712, 692333, 774967, 774974, 774976, 775995, 776239, 776468, 776570, 776819,
-    980608, 980638, 980644, 980649, 980652, 980655, 980659, 980680, 980681, 980701,
-    1311940, 1311955, 1311985, 1312038, 1312059, 1312147, 1312149, 1312168, 1383037, 1383090,
-    1383152, 1383225, 1383448, 1477623, 1477640, 1477645, 1477657, 1477728, 1477729, 1477735,
-    1477736, 1477815, 1477816]
+const forest_addressV2 = "0x9e894cd5dCC5Bad1eD3663077871d9D010f654b5";
+const forest_addressV1 = "0xb37d3d79ea86B0334d9322c695339D577A3D57be";
+const rarity_address = "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb";
 
 const web3 = new Web3(config.network.rpc);
-const forest = new web3.eth.Contract(FOREST_ABI, forest_address);
+const rarity = new web3.eth.Contract(RARITY_ABI, rarity_address);
+const forestV2 = new web3.eth.Contract(FOREST_ABI_V2, forest_addressV2);
+const forestV1 = new web3.eth.Contract(FOREST_ABI_V1, forest_addressV1);
 
 web3.eth.accounts.wallet.add(config.my_private_key);
 
@@ -26,21 +26,21 @@ async function getNonce() {
     return nonce;
 }
 
-async function startResearch(summoner, nonce) {
+async function startResearch(contract, summoner, nonce) {
     try {
-        let estimated_gas = await forest.methods.startResearch(summoner, config.days).estimateGas(
+        let estimated_gas = await contract.methods.startResearch(summoner, config.days).estimateGas(
             {
                 from: config.myAddress,
                 gas: '179043'
             });
-        console.log(`${summoner}: Estimated gas required is ${estimated_gas}.`);
+        // console.log(`${summoner}: Estimated gas required is ${estimated_gas}.`);
     } catch (error) {
         console.log(`${summoner}: The gas required for the transaction is too high, summoner is probably already in the forest.`);
         return;
     }
 
     let gas_price = await web3.eth.getGasPrice();
-    console.log(`${summoner}: gas price: ${gas_price}.`)
+    // console.log(`${summoner}: gas price: ${gas_price}.`)
 
     if ('maximum_gas_price' in config) {
         var max_price = web3.utils.toWei(config.maximum_gas_price.toString(), "Gwei")
@@ -52,34 +52,61 @@ async function startResearch(summoner, nonce) {
         console.log(`Current network gas price is ${web3.utils.fromWei(gas_price, "Gwei")} GWEI, above your price limit of ${web3.utils.fromWei(max_price, "Gwei")} GWEI. Not claiming.`);
         return;
     }
+    if (!config.dry_run) {
+        await contract.methods.startResearch(summoner, config.days)
+            .send({
+                from: config.myAddress,
+                gasPrice: gas_price,
+                gas: "179043",
+                nonce: nonce
+            }).on('sent', () => {
+                // console.log(`${summoner}: Transaction submitted ...`)
+            }).on('transactionHash', (hash) => {
+                // console.log(`${summoner}: https://ftmscan.com/tx/${hash}`)
+            }).on('receipt', (receipt) => {
+                console.log(`#${summoner} is in the forest for ${config.days} days`);
+            })
+            .catch((error) => {
+                console.log(`${summoner}: Error`, error)
+            });
+    } else {
+        console.log(`#${summoner} is in the forest for ${config.days} days`);
+    }
 
-    await forest.methods.startResearch(summoner, config.days)
-        .send({
-            from: config.myAddress,
-            gasPrice: gas_price,
-            gas: "179043",
-            nonce: nonce
-        }).on('sent', () => {
-            console.log(`${summoner}: Transaction submitted ...`)
-        }).on('transactionHash', (hash) => {
-            console.log(`${summoner}: https://ftmscan.com/tx/${hash}`)
-        }).on('receipt', (receipt) => {
-            console.log(`${summoner}: Summoner is in the forest!`)
-        })
-        .catch((error) => {
-            console.log(`${summoner}: Error`, error)
+}
+
+async function fetchMySummoners() {
+    return fetch(`https://api.ftmscan.com/api?module=account&action=tokennfttx&contractaddress=${rarity_address}&address=${config.myAddress}&apikey=${config.ftmscan_apikey}`)
+        .then(res => res.json())
+        .then(json => {
+            return Array.from(new Set(json.result.map(x => parseInt(x.tokenID))));
+        });
+}
+
+async function getLevel(summoner) {
+    return rarity.methods.level(summoner).call({from: config.myAddress})
+        .then(function (level) {
+            return level;
         });
 }
 
 export default class Forest {
     async run() {
         console.log(`Sending summoners exploring the forest...`);
-        let nonce = await getNonce()
-        console.log("nonce", nonce)
-        summoners.forEach(summoner => {
-            console.log(`${summoner}: Sending summoner in the forest...`);
-            startResearch(summoner, nonce++);
-        })
+        let nonce = await getNonce();
+        console.log("nonce", nonce);
+
+        let summoners = await fetchMySummoners();
+
+
+        for (const summoner of summoners) {
+            let level = await getLevel(summoner);
+            if (level >= 2) {
+                console.log(`Sending summoner ${summoner} level ${level} in the forests (V1 + V2)...`);
+                await startResearch(forestV1, summoner, nonce++);
+                await startResearch(forestV2, summoner, nonce++);
+            }
+        }
     };
 
 }
