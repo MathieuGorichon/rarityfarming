e0b8076e4974952dc2fd3acc9995cea049e833c9
menfin <menfin.swgoh@gmail.com>
2021-09-29 11:36:29 +0200

    meilleure gestion du nonce + discover

--- app.mjs
+++ app.mjs
@@ -4,13 +4,19 @@ import { Command } from 'commander/esm.mjs';
 import Forest from './forest.mjs';
 import TransferCraftingMaterials from "./transfer_crafting_materials.mjs";
 import TransferGold from "./transfer_gold.mjs";
+import Market from "./market.mjs";
 
 const program = new Command();
 
 program
-    .command("forest")
+    .command("forest-go")
     .description("Send summoners to the forest")
-    .action(()=> new Forest().run());
+    .action(()=> new Forest().go());
+
+program
+    .command("forest-disc")
+    .description("Send summoners to the forest")
+    .action(()=> new Forest().disc());
 
 program
     .command("transfer-mats")
@@ -21,5 +27,13 @@ program
     .command("transfer-gold")
     .description("Transfer gold from farmers to crafter")
     .action(()=> new TransferGold().run());
+
+program
+    .command("market-listed")
+    .action(()=> new Market().listed());
+
+program
+    .command("market-sellings")
+    .action(()=> new Market().sellings());
 
 program.parse(process.argv);
--- forest.mjs
+++ forest.mjs
@@ -4,7 +4,6 @@
 import FOREST_ABI_V2 from "./forestabi_v2.json";
 import FOREST_ABI_V1 from "./forestabi_v1.json";
 import RARITY_ABI from "./rarityabi.json";
-import {randomBytes} from "crypto";
 import Web3 from 'web3';
 
 import config from './config.json'
@@ -21,12 +20,13 @@ const forestV1 = new web3.eth.Contract(FOREST_ABI_V1, forest_addressV1);
 
 web3.eth.accounts.wallet.add(config.my_private_key);
 
-async function getNonce() {
-    const nonce = await web3.eth.getTransactionCount(config.myAddress);
-    return nonce;
+let nonce = 0;
+
+async function initNonce() {
+    nonce = await web3.eth.getTransactionCount(config.myAddress);
 }
 
-async function startResearch(contract, summoner, nonce) {
+async function startResearch(contract, summoner) {
     try {
         let estimated_gas = await contract.methods.startResearch(summoner, config.days).estimateGas(
             {
@@ -35,7 +35,7 @@ async function startResearch(contract, summoner, nonce) {
             });
         // console.log(`${summoner}: Estimated gas required is ${estimated_gas}.`);
     } catch (error) {
-        console.log(`${summoner}: The gas required for the transaction is too high, summoner is probably already in the forest.`);
+        // console.log(`${summoner}: The gas required for the transaction is too high, summoner is probably already in the forest.`);
         return;
     }
 
@@ -53,27 +53,77 @@ async function startResearch(contract, summoner, nonce) {
         return;
     }
     if (!config.dry_run) {
-        await contract.methods.startResearch(summoner, config.days)
+        contract.methods.startResearch(summoner, config.days)
             .send({
                 from: config.myAddress,
                 gasPrice: gas_price,
                 gas: "179043",
                 nonce: nonce
             }).on('sent', () => {
-                // console.log(`${summoner}: Transaction submitted ...`)
-            }).on('transactionHash', (hash) => {
-                // console.log(`${summoner}: https://ftmscan.com/tx/${hash}`)
-            }).on('receipt', (receipt) => {
-                console.log(`#${summoner} is in the forest for ${config.days} days`);
-            })
-            .catch((error) => {
-                console.log(`${summoner}: Error`, error)
-            });
+            // console.log(`${summoner}: Transaction submitted ...`)
+        }).on('transactionHash', (hash) => {
+            console.log(`${summoner}: https://ftmscan.com/tx/${hash}`)
+        }).on('receipt', (receipt) => {
+            console.log(`#${summoner} is in the forest for ${config.days} days`);
+        }).catch((error) => {
+            console.log(`${summoner}: Error`, error)
+        });
+        nonce++;
     } else {
         console.log(`#${summoner} is in the forest for ${config.days} days`);
     }
-
 }
+
+async function discover(contract, summoner) {
+    try {
+        let estimated_gas = await contract.methods.discover(summoner).estimateGas(
+            {
+                from: config.myAddress,
+                gas: '179043'
+            });
+        // console.log(`${summoner}: Estimated gas required is ${estimated_gas}.`);
+    } catch (error) {
+        // console.log(`${summoner}: The gas required for the transaction is too high, summoner is probably already in the forest.`);
+        return;
+    }
+
+    let gas_price = await web3.eth.getGasPrice();
+    // console.log(`${summoner}: gas price: ${gas_price}.`)
+
+    if ('maximum_gas_price' in config) {
+        var max_price = web3.utils.toWei(config.maximum_gas_price.toString(), "Gwei")
+    } else {
+        var max_price = web3.utils.toWei("1", "Gwei")
+    }
+
+    if (parseFloat(gas_price) > max_price) {
+        console.log(`Current network gas price is ${web3.utils.fromWei(gas_price, "Gwei")} GWEI, above your price limit of ${web3.utils.fromWei(max_price, "Gwei")} GWEI. Not claiming.`);
+        return;
+    }
+    if (!config.dry_run) {
+        // console.log(`[#${summoner}] send with gas price: ${gas_price} and nonce: ${nonce}.`);
+
+        contract.methods.discover(summoner)
+            .send({
+                from: config.myAddress,
+                gasPrice: gas_price,
+                gas: "179043",
+                nonce: nonce
+            }).on('sent', () => {
+            // console.log(`${summoner}: Transaction submitted ...`)
+        }).on('transactionHash', (hash) => {
+            console.log(`${summoner}: https://ftmscan.com/tx/${hash}`)
+        }).on('receipt', (receipt) => {
+            console.log(`#${summoner} discovered a treasure !`);
+        }).catch((error) => {
+            console.log(`${summoner}: Error`, error)
+        });
+        nonce++;
+    } else {
+        console.log(`#${summoner} discovered a treasure !`);
+    }
+}
+
 
 async function fetchMySummoners() {
     return fetch(`https://api.ftmscan.com/api?module=account&action=tokennfttx&contractaddress=${rarity_address}&address=${config.myAddress}&apikey=${config.ftmscan_apikey}`)
@@ -91,22 +141,34 @@ async function getLevel(summoner) {
 }
 
 export default class Forest {
-    async run() {
+    async go() {
         console.log(`Sending summoners exploring the forest...`);
-        let nonce = await getNonce();
+        await initNonce();
         console.log("nonce", nonce);
 
         let summoners = await fetchMySummoners();
 
-
         for (const summoner of summoners) {
             let level = await getLevel(summoner);
             if (level >= 2) {
-                console.log(`Sending summoner ${summoner} level ${level} in the forests (V1 + V2)...`);
-                await startResearch(forestV1, summoner, nonce++);
-                await startResearch(forestV2, summoner, nonce++);
+                await startResearch(forestV1, summoner);
+                await startResearch(forestV2, summoner);
             }
         }
-    };
+    }
 
+    async disc() {
+        console.log(`Discover what summoners found in the forest...`);
+        await initNonce();
+        console.log("nonce", nonce);
+
+        let summoners = await fetchMySummoners();
+        for (const summoner of summoners) {
+            let level = await getLevel(summoner);
+            if (level >= 2) {
+                await discover(forestV1, summoner);
+                await discover(forestV2, summoner);
+            }
+        }
+    }
 }
